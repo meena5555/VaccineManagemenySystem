8614177d6810da95cf9dbc9e29c7b987147d1948
meena5555 <meenakshivarma12345@gmail.com>
2022-08-03 11:09:40 +0530

    4cmm

--- Homepage.java
+++ Homepage.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Homepage extends JFrame {
 
@@ -35,8 +36,15 @@ public class Homepage extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 5, 426, 102);
+		contentPane.add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(43, 147, 85, 21);
+		contentPane.add(btnNewButton);
 	}
-
 }
